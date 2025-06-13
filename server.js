bea08bbca28caa1871bffcf2ec9d414d72ebff20
quoc khanh <quockhanh@bhdvn.com>
2025-06-13 16:10:49 +0700

    ww

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port,'0.0.0.0', () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app lisssstening on port ${port}`)
 })
